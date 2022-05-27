b70ba39213f60a66242fb359f066ff260191f5cf
tpxxn <351765204@qq.com>
2022-05-28 00:58:40 +0800

    [feature]伊卡洛斯无限能量

---
+++ IcarusInfiniteEnergy/IcarusInfiniteEnergy.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Reflection;
+// using xiaoye97;
+using BepInEx;
+using HarmonyLib;
+using UnityEngine;
+
+namespace IcarusInfiniteEnergy
+{
+    // [BepInDependency("me.xiaoye97.plugin.Dyson.LDBTool", BepInDependency.DependencyFlags.HardDependency)]
+    [BepInPlugin("tpxxn.plugin.Dyson.IcarusInfiniteEnergy", "IcarusInfiniteEnergy", "1.0")]
+    public class IcarusInfiniteEnergy : BaseUnityPlugin
+    {
+        void Start()
+        {
+            //LDBTool.EditDataAction += Edit;
+        }
+
+        void Update()
+        {
+            if (GameMain.data != null && GameMain.data.mainPlayer != null)
+            {
+                GameMain.data.mainPlayer.mecha.coreEnergyCap = 1e8;
+                GameMain.data.mainPlayer.mecha.coreEnergy = 1e8;
+            }
+        }
+
+        //void Edit(Proto proto)
+        //{
+        //    if (proto is RecipeProto && proto.ID == 67)
+        //    {
+        //        var recipe = proto as RecipeProto;
+        //        recipe.Items[1] = 1113;
+        //    }
+        //}
+    }
+}
